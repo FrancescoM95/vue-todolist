98b345a946a13a7eca9627caddf023f41ca4b3ef
FrancescoM95 <mon95fran@gmail.com>
2024-01-23 15:13:21 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('Vue OK', Vue);
+
+const { createApp } = Vue;
+const app = createApp({
+    name: 'VueToDoList',
+    data: () => ({
+
+    }),
+    computed: {
+
+    },
+    methods: {
+
+    },
+});
+
+app.mount('#root');
