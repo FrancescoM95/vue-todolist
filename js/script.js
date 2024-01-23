024c12ecd57acd336cc63616bc635b6d0986ac62
FrancescoM95 <mon95fran@gmail.com>
2024-01-23 15:46:51 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ const { createApp } = Vue;
 const app = createApp({
     name: 'VueToDoList',
     data: () => ({
-
+        tasks: [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ],
     }),
     computed: {
 
